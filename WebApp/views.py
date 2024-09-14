f006a0218d7f8ebc239c71db66a91f4120ac8594
Vincent <vincentlyonsvl@gmail.com>
2024-09-14 19:41:00 -0400

    Changed site layout to flowbite.

--- WebApp/views.py
+++ WebApp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {'siteTitle': 'CardCollector - Dev'})
+    return render(request, 'index.html', {'siteName': 'CardCollector', 'pageTitle': 'Home'})
