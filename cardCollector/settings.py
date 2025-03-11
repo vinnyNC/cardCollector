68fc64e1d4e1981f04028151570773104d19d2d3
Vincent Lyons <vincentlyonsvl@gmail.com>
2025-03-11 09:07:05 -0400

    Simplify dependencies and remove unused database config.
    Updated `requirements.txt` to pin exact versions for dependencies, ensuring consistency across environments. Removed an unused SQLite database configuration from `settings.py` to streamline the project setup.

--- cardCollector/settings.py
+++ cardCollector/settings.py
@@ -81,10 +81,6 @@ WSGI_APPLICATION = 'cardCollector.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default2': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'cardcollector_test',
