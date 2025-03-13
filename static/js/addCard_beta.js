0170d569b78c004a7585a8c92f1255416d5fa837
Vincent Lyons <vincentlyonsvl@gmail.com>
2025-03-13 14:43:51 -0400

    Extract stepper into a separate partial template.
    Moved the stepper HTML into its own partial template to improve code modularity and reusability. Updated the main add_card.html file to include the new partial. Also initialized basic stepper functionality in addCard_beta.js for handling step changes.

---
+++ static/js/addCard_beta.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('btnPrev').disabled = true;
+    });
+
+/*
+Stepper functions
+ */
+
+
+let stepperCurrentStep = 1;
+
+
+function stepperChangeStep(step) {
+    let listItemActiveClasses = ['text-blue-600', 'dark:text-blue-500'];
+    let listItemChildActiveClasses = ['border-blue-600', 'dark:border-blue-500'];
+    let listItemChildInactiveClasses = ['border-gray-500', 'dark:border-gray-400'];
+
+
+}
