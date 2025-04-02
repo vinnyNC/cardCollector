ff0b067a3c5fcac3d30c0c7378f36525e7033d6e
Vincent <13386908+vinnyNC@users.noreply.github.com>
2025-04-02 17:50:01 -0400

    Add database analysis documentation and optimize URL structure
    Introduced a detailed guide for analyzing and enhancing database schemas, including SQL queries for schema inspections. Additionally, improved URL formatting and consistency in the Django project.

--- cardCollector/urls.py
+++ cardCollector/urls.py
@@ -1,14 +1,15 @@
 import django.conf
 import django.conf.urls.static
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 import cardCollector.settings
 from WebApp.views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index, name='index'),
+                  path('admin/', admin.site.urls),
+                  path('', index, name='index'),
 
                   # Authentication
                   path('login/', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
@@ -16,13 +17,14 @@ urlpatterns = [
                   path('register/', register, name='register'),
                   path('profile/', profile, name='profile'),
 
-    # API
-    path('api/sets', set_search_name, name='set_name_search'),
-    path('api/cards', card_num_search, name='card_num_search'),
-    path('api/inserts?insertName=<str:insert_name>&setID=<int:set_id>', insert_name_search, name='insert_name_search'),
-    path('api/inserts/all/<int:set_id>', set_all_inserts, name='set_all_inserts'),
+                  # API
+                  path('api/sets', set_search_name, name='set_name_search'),
+                  path('api/cards', card_num_search, name='card_num_search'),
+                  path('api/inserts?insertName=<str:insert_name>&setID=<int:set_id>', insert_name_search,
+                       name='insert_name_search'),
+                  path('api/inserts/all/<int:set_id>', set_all_inserts, name='set_all_inserts'),
 
-    # Test Links
-    path('add_card', add_card_1, name='add_card_1'),
+                  # Test Links
+                  path('add_card', add_card_1, name='add_card_1'),
               ] + django.conf.urls.static.static(django.conf.settings.MEDIA_URL,
                                                  document_root=cardCollector.settings.MEDIA_ROOT)
