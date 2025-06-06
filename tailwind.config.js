3d41bc1f3c137d6d24c355407752f4d161301b82
vinnync <vincentlyonsvl@gmail.com>
2025-06-06 14:57:42 -0400

    Minor changes to image modal for step 2.
    ImageModal.html still broken, needs work.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        './**/templates/**/*.html', // Adjust this path to match your project structure
+        './**/static/js/**/*.js',   // If you have JS files that might include Tailwind classes
+        './node_modules/flowbite/**/*.js' // If using Flowbite JS components
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require('flowbite/plugin')
+    ],
 }
-
